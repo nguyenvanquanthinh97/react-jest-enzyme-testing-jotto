4a43e3ef356d543c15d0f957d8f126c257556fd1
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-08-03 12:07:36 +0700

    test case for setSecretWord in App mount

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import React from 'react';
 import './App.css';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
+import hookActions from './actions/hookActions';
+
+/**
+ * Reducer to update state, called update state by dispatch
+ * @param {object} state - existing state
+ * @param {object} action - contains "type" and "payload" properties for the state update 
+ * 												for example: {type: 'setSecretWord', payload: 'party'}
+ * @returns {object} - new state
+ */
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'setSecretWord':
+			return { ...state, secretWord: action.payload };
+		default:
+			throw new Error(`Invalid action type: ${action.type}`);
+	}
+};
 
 function App() {
+	const [ state, dispatch ] = React.useReducer(reducer, { secretWord: null });
+
+	const setSecretWord = (secretWord) => dispatch({ type: 'setSecretWord', payload: secretWord });
+
+	React.useEffect(() => {
+		hookActions.getSecretWord(setSecretWord);
+	}, []);
+
 	return (
 		<div className="container text-center" data-test="component-app">
 			<h1>Jotto</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import App from './App';
 import { findByTestAttr } from './test/utils';
+import hookActions from './actions/hookActions';
+
+const mockSecretWord = jest.fn();
 
 /**
  * setup function for App
@@ -11,7 +14,15 @@ import { findByTestAttr } from './test/utils';
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}, state = null) => {
-	const wrapper = shallow(<App {...props} />);
+	mockSecretWord.mockClear();
+	hookActions.getSecretWord = mockSecretWord;
+
+	//this use for shallow
+	// React.useEffect = jest.fn(() => hookActions.getSecretWord());
+	// const wrapper = shallow(<App {...props} />);
+
+	//this use for mount
+	const wrapper = mount(<App {...props} />);
 	if (state) wrapper.setState(state);
 	return wrapper;
 };
@@ -20,4 +31,21 @@ test('render App without errors', () => {
 	const wrapper = setup();
 	const component = findByTestAttr(wrapper, 'component-app');
 	expect(component.length).toBe(1);
+});
+
+describe('getSecretWord called', () => {
+	test('getSecretWord gets called on App mount', () => {
+		setup();
+		expect(mockSecretWord).toHaveBeenCalled();
+	});
+
+	test('setSecretWord is not called on update', () => {
+		const wrapper = setup();
+
+		//refresh mockSecretWord
+		mockSecretWord.mockClear();
+
+		wrapper.update();
+		expect(mockSecretWord).not.toHaveBeenCalled();
+	});
 });
